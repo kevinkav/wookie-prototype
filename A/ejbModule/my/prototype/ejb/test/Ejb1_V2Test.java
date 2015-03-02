aa1bed6e330a591d9a622aff5a590776b0fdc3e2
eeikkah <kevin.kavanagh@ericsson.com>
2015-03-02 15:44:26 +0000

    Finished ejb1_v2 junit testing

--- A/ejbModule/my/prototype/ejb/test/Ejb1_V2Test.java
+++ A/ejbModule/my/prototype/ejb/test/Ejb1_V2Test.java
@@ -1,15 +1,22 @@
 package my.prototype.ejb.test;
 
-import static my.prototype.common.Constants.IRELAND;
-import static my.prototype.common.Constants.USA;
-import static my.prototype.common.Constants.HARRISON_FORD;
 import static my.prototype.common.Constants.CAST_ID;
+import static my.prototype.common.Constants.FILM_ID;
+import static my.prototype.common.Constants.HARRISON_FORD;
+import static my.prototype.common.Constants.IRELAND;
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import java.rmi.RemoteException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.ejb.CreateException;
+import javax.naming.NamingException;
 import javax.persistence.EntityManager;
+import javax.persistence.Query;
 
 import my.prototype.ejb.CorbaUtil;
 import my.prototype.ejb.Ejb1_V2;
@@ -28,14 +35,16 @@ import org.mockito.runners.MockitoJUnitRunner;
 
 @RunWith(MockitoJUnitRunner.class)
 public class Ejb1_V2Test {
-	
-    private static final String EJB2_ADDRESS = 
-    		"corbaname:iiop:localhost:3628#" + Ejb2RemoteObject.EJB2_BINDING_JNDI;
 
-    @Mock
-    private Film mockFilm;
-    @Mock
-    private Cast mockCast;
+	private static final String EJB2_ADDRESS = 
+			"corbaname:iiop:localhost:3628#" + Ejb2RemoteObject.EJB2_BINDING_JNDI;
+
+	@Mock
+	private Film mockFilm;
+	@Mock
+	private Cast mockCast;
+	@Mock
+	private Query mockQuery;
 	@Mock
 	private EntityManager mockEm;
 	@Mock
@@ -44,9 +53,9 @@ public class Ejb1_V2Test {
 	private CorbaUtil mockCorbaUtil;
 	@InjectMocks
 	private Ejb1_V2 ejb_v2 = new Ejb1_V2();
-	
+
 	private Film expectedFilm;
-	
+
 
 	/**
 	 * @throws java.lang.Exception
@@ -66,14 +75,14 @@ public class Ejb1_V2Test {
 	public void tearDown() throws Exception {
 	}
 
-	
+
 	@Test
 	public void test_setUp() throws Exception {
 		String actualResult = ejb_v2.setUp();
 		verify(mockEm).persist(any(Film.class));
-		Assert.assertTrue("Expected film created message", actualResult.contains("created"));
+		Assert.assertTrue("Expected a film created message!", actualResult.contains("created"));
 	}
-	
+
 	@Test
 	public void test_runTest() throws Exception{
 		when(mockEm.find(any(Class.class), any(Object.class))).thenReturn(mockFilm);
@@ -83,14 +92,94 @@ public class Ejb1_V2Test {
 		when(mockFilm.getCast()).thenReturn(mockCast);
 		when(mockCast.getLeadActor()).thenReturn(HARRISON_FORD);
 		when(mockCast.getId()).thenReturn(CAST_ID);
-		
+
 		String testResult = ejb_v2.runTest();
-		
-		Assert.assertEquals("Should get passed message!", "Passed", testResult);
-		verify(mockEjb2).getCountryOfOrigin(1l);
-		verify(mockEjb2, times(1)).createCast(1l);;
-		
+
+		Assert.assertEquals("Expected a pass message!", "Passed", testResult);
+		verify(mockEjb2).getCountryOfOrigin(FILM_ID);
+		verify(mockEjb2, times(1)).createCast(FILM_ID);
+		verify(mockFilm, times(2)).getCountryOfOrigin();
+		verify(mockCorbaUtil).getEjb2RemoteObject(EJB2_ADDRESS);
+		verify(mockCast).getId();
+		verify(mockCast).getLeadActor();
+
 	}
+
+	@Test (expected = NamingException.class)
+	public void test_runTest_NamingException() throws Exception{
+
+		when(mockEm.find(any(Class.class), any(Object.class))).thenReturn(mockFilm);
+		when(mockFilm.getCountryOfOrigin()).thenReturn(IRELAND);
+		when(mockCorbaUtil.getEjb2RemoteObject(EJB2_ADDRESS)).thenThrow(new NamingException());
+		when(mockEjb2.getCountryOfOrigin(1l)).thenReturn(IRELAND);
+
+		ejb_v2.runTest();
+	}
+	
+	@Test (expected = CreateException.class)
+	public void test_runTest_CreateException() throws Exception{
+
+		when(mockEm.find(any(Class.class), any(Object.class))).thenReturn(mockFilm);
+		when(mockFilm.getCountryOfOrigin()).thenReturn(IRELAND);
+		when(mockCorbaUtil.getEjb2RemoteObject(EJB2_ADDRESS)).thenThrow(new CreateException());
+		when(mockEjb2.getCountryOfOrigin(1l)).thenReturn(IRELAND);
+
+		ejb_v2.runTest();
+	}
+	
+	@Test (expected = RemoteException.class)
+	public void test_runTest_RemoteException() throws Exception{
+
+		when(mockEm.find(any(Class.class), any(Object.class))).thenReturn(mockFilm);
+		when(mockFilm.getCountryOfOrigin()).thenReturn(IRELAND);
+		when(mockCorbaUtil.getEjb2RemoteObject(EJB2_ADDRESS)).thenThrow(new RemoteException());
+		when(mockEjb2.getCountryOfOrigin(1l)).thenReturn(IRELAND);
+
+		ejb_v2.runTest();
+	}
+
+	@Test
+	public void test_getCountryOfOrigin(){
+		
+		when(mockEm.find(any(Class.class), any(Object.class))).thenReturn(mockFilm);
+		when(mockFilm.getCountryOfOrigin()).thenReturn(IRELAND);
+		
+		String countryOfOrigin = ejb_v2.getCountryOfOrigin(FILM_ID);
+
+		Assert.assertEquals("Expected to get " + IRELAND,  IRELAND, countryOfOrigin);
+		verify(mockEm).find(my.prototype.entity.Film.class, 1l);
+		verify(mockFilm).getCountryOfOrigin();
+	}
+	
+	@Test
+	public void test_AddCastToFilm(){
+		
+		when(mockEm.find(any(Class.class), any(Object.class))).thenReturn(mockFilm);
+		
+		ejb_v2.addCastToFilm();
+		
+		verify(mockEm).find(my.prototype.entity.Film.class, 1l);
+		verify(mockFilm).setCast(any(Cast.class));
+		verify(mockEm, times(2)).persist(any(Object.class));
+	}
+	
+	
+	@Test
+	public void test_tearDown(){
+		when(mockEm.createQuery(any(String.class))).thenReturn(mockQuery);
+		List<Cast> castList = new ArrayList<>();
+		castList.add(mockCast);
+		List<Film> filmList = new ArrayList<>();
+		filmList.add(mockFilm);
+		when(mockQuery.getResultList()).thenReturn(castList).thenReturn(filmList); // stubbing consecutive calls
+		when(mockFilm.getId()).thenReturn(FILM_ID);
+		when(mockCast.getId()).thenReturn(CAST_ID);
+		String message = ejb_v2.tearDown();
+
+		verify(mockEm, times(2)).remove(any(Long.class));
+		Assert.assertTrue("Expected deleted message!", message.contains("Deleted"));
+	}
+	
 	
 	private Film createFilm() {
 		Film film = new Film();
@@ -102,6 +191,6 @@ public class Ejb1_V2Test {
 		film.setYearOfRelease(1977);
 		return film;
 	}
-	
+
 
 }
